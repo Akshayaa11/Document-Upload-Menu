81a11ae5f45c299b4eee424d2524ce9fafd40daa
Akshayaa11 <94050165+Akshayaa11@users.noreply.github.com>
2021-11-11 11:35:19 +0530

    Update document-upload-management.controller.js

--- document-upload-management.controller.js
+++ document-upload-management.controller.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('nimbusApp')
+        .module('csplDocumentManagementMenuModule')
         .controller('DocumentUploadManagementController', DocumentUploadManagementController);
 
         DocumentUploadManagementController.$inject = ['$scope'];
